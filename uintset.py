fd020c505f0a6873f455384a37106b0d0415e5f4
Luciano Ramalho <luciano@ramalho.org>
2018-04-22 21:40:02 -0300

    Implemented __eq__, copy, union, intersection

--- uintset.py
+++ uintset.py
@@ -1,17 +1,17 @@
 import array
-
+import copy
 
 class UintSet():
 
     def __init__(self, it=None):
-        self.words = array.array('I')
-        self.len = 0
+        self._words = array.array('L')
+        self._len = 0
         if it is not None:
             for i in it:
                 self.add(i)
 
     def __len__(self):
-        return self.len
+        return self._len
 
     def add(self, elem):
         if elem < 0:
@@ -19,18 +19,18 @@ class UintSet():
         if elem in self:
             return
         word, bit = elem // 64, elem % 64
-        while word >= len(self.words):
-            self.words.append(0)
-        self.words[word] |= 1 << bit
-        self.len += 1
+        while word >= len(self._words):
+            self._words.append(0)
+        self._words[word] |= (1 << bit)
+        self._len += 1
 
     def __contains__(self, elem):
         word, bit = elem // 64, elem % 64
-        return (word < len(self.words) and
-                (self.words[word] >> bit) & 1 == 1)
+        return (word < len(self._words) and
+                (self._words[word] >> bit) & 1 == 1)
 
     def __iter__(self):
-        for i, word in enumerate(self.words):
+        for i, word in enumerate(self._words):
             if word == 0:
                 continue
             for j in range(64):
@@ -45,3 +45,61 @@ class UintSet():
             parts.append('}')
         parts.append(')')
         return ''.join(parts)
+
+    def __eq__(self, other):
+        return (len(self) == len(other)
+                and self._words == other._words)
+
+    def copy(self):
+        clone = UintSet()
+        clone._words = copy.copy(self._words)
+        clone._len = self._len
+        return clone
+
+    def union(self, other):
+        if len(self) > len(other):
+            longer = self
+            shorter = other
+        else:
+            longer = other
+            shorter = self
+        new = longer.copy()
+        n_words, s_words = new._words, shorter._words
+        for i in range(len(s_words)):
+            n_word, s_word = n_words[i], s_words[i]
+            if n_word != s_word:
+                old_count = bit_count(n_word)
+                n_word |= s_word
+                new._len += bit_count(n_word) - old_count
+                n_words[i] = n_word
+        return new
+
+    def intersection(self, other):
+        if len(self) > len(other):
+            longer = self
+            shorter = other
+        else:
+            longer = other
+            shorter = self
+        new = shorter.copy()
+        n_words, l_words = new._words, longer._words
+        for i in range(len(n_words)):
+            n_word, l_word = n_words[i], l_words[i]
+            if n_word != l_word:
+                old_count = bit_count(n_word)
+                n_word &= l_word
+                new._len += bit_count(n_word) - old_count
+                n_words[i] = n_word
+        # trim redundant words at end of array
+        while new._words and new._words[-1] == 0:
+            del new._words[-1]
+        return new
+
+
+def bit_count(n):
+    count = 0
+    while n:
+        count += n & 1
+        n >>= 1
+    return count
+
--- uintset_test.py
+++ uintset_test.py
@@ -1,6 +1,7 @@
+import collections
 import pytest
 
-from uintset import UintSet
+from uintset import UintSet, bit_count
 
 
 def test_new():
@@ -9,13 +10,13 @@ def test_new():
 
 
 def test_new_from_iterable():
-    s = UintSet([1, 2, 3])
+    s = UintSet([1, 100, 3])  # beyond word 0
     assert len(s) == 3
 
 
 def test_add():
     s = UintSet()
-    s.add(1)
+    s.add(0)
     assert len(s) == 1
 
 
@@ -52,3 +53,77 @@ def test_repr_empty():
 def test_repr():
     s = UintSet([1, 5, 0, 3, 2, 4])
     assert repr(s) == 'UintSet({0, 1, 2, 3, 4, 5})'
+
+
+def test_eq():
+    TC = collections.namedtuple('TestCase', 's1 s2 want')
+    test_cases = [
+        TC(UintSet(), UintSet(), True),
+        TC(UintSet([1]), UintSet(), False),
+        TC(UintSet(), UintSet([1]), False),
+        TC(UintSet([1, 2, 100]), UintSet([100, 2, 1]), True), # beyond word 0
+        TC(UintSet([1, 100]), UintSet([1, 101]), False),
+        TC(UintSet([1, 100]), UintSet([1, 100, 1000]), False),
+    ]
+    for t in test_cases:
+        assert (t.s1 == t.s2) is t.want
+
+
+def test_copy():
+    test_cases = [
+        UintSet(),
+        UintSet([1]),
+        UintSet([1, 2]),
+        UintSet([1, 100]),  # beyond word 0
+    ]
+    for s1 in test_cases:
+        s2 = s1.copy()
+        assert s1 == s2
+
+
+def test_union():
+    TC = collections.namedtuple('TestCase', 's1 s2 want')
+    test_cases = [
+        TC(UintSet(), UintSet(), UintSet()),
+        TC(UintSet([1]), UintSet(), UintSet([1])),
+        TC(UintSet(), UintSet([1]), UintSet([1])),
+        TC(UintSet([1, 100]), UintSet([100, 1]), UintSet([100, 1])), # beyond word 0
+        TC(UintSet([1, 100]), UintSet([2]), UintSet([1, 2, 100])),
+    ]
+    for t in test_cases:
+        got = t.s1.union(t.s2)
+        assert len(got) == len(t.want)
+        assert got == t.want
+
+
+def test_intersection():
+    test_cases = [
+        (UintSet(), UintSet(), UintSet()),
+        (UintSet([1]), UintSet(), UintSet()),
+        (UintSet([1]), UintSet([1]), UintSet([1])),
+        (UintSet([1, 100]), UintSet([100, 1]), UintSet([100, 1])), # beyond word 0
+        (UintSet([1, 100]), UintSet([2]), UintSet()),
+        (UintSet([1, 2, 3, 4]), UintSet([2, 3, 5]), UintSet([2, 3])),
+    ]
+    for s1, s2, want in test_cases:
+        got = s1.intersection(s2)
+        assert len(got) == len(want)
+        assert got == want
+
+
+def test_bitcount():
+    test_cases = [
+        (0, 0),
+        (1, 1),
+        (2, 1),
+        (3, 2),
+        (10, 2),
+        (11, 3),
+        (63, 6),
+        (64, 1),
+        (2 ** 64-2, 63),
+        (2 ** 64-1, 64),
+        (2 ** 64, 1),
+    ]
+    for n, want in test_cases:
+        assert bit_count(n) == want
