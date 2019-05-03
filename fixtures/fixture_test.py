f78247fd0c1d0a29bf2e46f72abfaad13f4840cd
Luciano Ramalho <luciano@ramalho.org>
2019-05-02 23:44:24 -0400

    new implementation of uintset using a (long) int as an array of bits

--- fixtures/fixture_test.py
+++
@@ -1,9 +0,0 @@
-import math
-import pytest
-
-from fixture import Fixture
-
-@pytest.mark.skip(reason="WIP")
-def test_density_100pct():
-    fix = Fixture(20, 100)
-    assert math.isclose(fix.density(), 100)
